356a4288e3fe2b631bc1259595d9810f1b141878
lyginli <lyginli@tencent.com>
2024-07-17 11:02:25 +0800

    Fix complie error when building param server lib

--- cc/benchmark-dir/process_ycsb.cc
+++ cc/benchmark-dir/process_ycsb.cc
@@ -7,32 +7,17 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <random>
+const int N = 10000000;
 
 int main(int argc, char* argv[]) {
-  if (argc != 3) {
-    fprintf(stderr, "Requires two arguments: file copied from, file copied to.\n");
-    exit(-1);
-  }
-
-  std::string from_filename{ argv[1] };
-  std::string to_filename{ argv[2] };
-
-  std::ifstream from_file{ from_filename };
-  std::ofstream to_file{ to_filename };
+  std::ofstream to_file{ "load.txt" };
 
   const std::string prefix{ "usertable user" };
-  
-  while (!from_file.eof()) {
-    char buffer[256];
-    from_file.clear();
-    from_file.getline(buffer, sizeof(buffer));
-    std::string line{ buffer };
-    std::string::size_type pos = line.find(prefix);
-    if (pos == std::string::npos) {
-      continue;
-    }
-    line = line.substr(pos + prefix.size());
-    uint64_t key = stol(line);
+  std::mt19937_64 generator_;
+  std::uniform_int_distribution<uint64_t> dist_(0,N);
+  for(int i=0; i<N; ++i) {
+    uint64_t key = dist_(generator_);
 
     to_file.write(reinterpret_cast<char*>(&key), sizeof(key));
   }
